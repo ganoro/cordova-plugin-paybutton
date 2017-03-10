545699dee224318c4efdb5af4deb6f485220a10f
Hilary Barr <hilaryrosebarr@gmail.com>
2017-03-09 22:15:04 -0500

    Add native payworks function

--- src/ios/Paybutton.h
+++ src/ios/Paybutton.h
@@ -1,9 +1,11 @@
 #import <Cordova/CDV.h>
+#import <UIKit/UIKit.h>
 #import <mpos-ui/mpos-ui.h>
 
 @interface Paybutton : CDVPlugin
 
 - (void) greet:(CDVInvokedUrlCommand*)command;
 - (void) transaction:(CDVInvokedUrlCommand*)command;
+- (IBAction)paymentButtonClicked:(id)sender;
 
 @end
--- src/ios/Paybutton.m
+++ src/ios/Paybutton.m
@@ -36,4 +36,69 @@
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
+
+
+
+- (IBAction)paymentButtonClicked:(id)sender {
+    MPUMposUi *ui = [MPUMposUi initializeWithProviderMode:MPProviderModeMOCK
+                                       merchantIdentifier:@"merchantIdentifier"
+                                           merchantSecret:@"merchantSecretKey"];
+    
+    
+    // Start with a mocked card reader:
+    MPAccessoryParameters *ap = [MPAccessoryParameters mockAccessoryParameters];
+
+    // When using the Bluetooth Miura Shuttle / M007 / M010, use the following parameters:
+    // MPAccessoryParameters *ap = [MPAccessoryParameters externalAccessoryParametersWithFamily:MPAccessoryFamilyMiuraMPI
+    //                                                                                 protocol:@"com.miura.shuttle"
+    //                                                                                optionals:nil];
+    
+    MPTransactionParameters *tp = [MPTransactionParameters chargeWithAmount:[NSDecimalNumber decimalNumberWithString:@"5.00"]
+                                                                   currency:MPCurrencyEUR
+                                                                  optionals:^(id<MPTransactionParametersOptionals>  _Nonnull optionals) {
+                                                                      optionals.subject = @"Bouquet of Flowers";
+                                                                      optionals.customIdentifier = @"yourReferenceForTheTransaction";
+                                                                  }];
+    
+    ui.configuration.terminalParameters = ap;
+    ui.configuration.summaryFeatures = MPUMposUiConfigurationSummaryFeatureSendReceiptViaEmail;
+    // Add this line, if you do also want to offer printed receipts
+    //ui.configuration.printerParameters =
+    //ui.configuration.summaryFeatures |= MPUMposUiConfigurationSummaryFeaturePrintReceipt;
+    // Add this if you would like to collect the customer signature on the printed merchant receipt
+    //ui.configuration.signatureCapture = MPUMposUiConfigurationSignatureCaptureOnReceipt;
+    
+    
+    UIViewController *viewController = [ui createTransactionViewControllerWithTransactionParameters:tp
+                                                                                          completed:^(UIViewController * _Nonnull controller, MPUTransactionResult result, MPTransaction * _Nullable transaction)
+                                        {
+                                            [self dismissViewControllerAnimated:YES completion:NULL];
+                                            
+                                            UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Result"
+                                                                                            message:@""
+                                                                                           delegate:nil
+                                                                                  cancelButtonTitle:nil
+                                                                                  otherButtonTitles:@"OK",nil];
+                                            
+                                            if (result == MPUTransactionResultApproved) {
+                                                alert.message = @"Payment was approved!";
+                                            } else {
+                                                alert.message = @"Payment was declined/aborted!";
+                                            }
+                                            
+                                            [alert show];
+                                        }];
+    
+    
+    UINavigationController *modalNav = [[UINavigationController alloc] initWithRootViewController:viewController];
+    modalNav.navigationBar.barStyle = UIBarStyleBlack;
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+        modalNav.modalPresentationStyle = UIModalPresentationFullScreen;
+    } else { // Show as Form on iPad
+        modalNav.modalPresentationStyle = UIModalPresentationFormSheet;
+    }
+    [self presentViewController:modalNav animated:YES completion:NULL];
+}
+
+
 @end
